e4460e1de7f12ad759d353d96a6cf68e3c546f51
bsfuji <fujimoto@bravesoft.co.jp>
2016-05-17 22:50:12 +0900

    mod lollipop以降の通知用アイコンを変更。

--- app/src/main/java/com/appvisor_event/master/modules/Gcm/GcmIntentService.java
+++ app/src/main/java/com/appvisor_event/master/modules/Gcm/GcmIntentService.java
@@ -6,7 +6,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -91,7 +90,7 @@ public class GcmIntentService extends IntentService
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
         {
-            builder.setSmallIcon(R.drawable.ic_launcher).setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher));
+            builder.setSmallIcon(R.drawable.ic_status);
         }
 
         builder.setContentIntent(contentIntent);

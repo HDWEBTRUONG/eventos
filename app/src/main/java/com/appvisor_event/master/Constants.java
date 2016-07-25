43179eaba51bb8eee27b708deca7d54be9a03565
kawa <birdhxc@gmail.com>
2016-07-25 16:53:15 +0900

    push　テスト　本番
    push　テスト　本番

--- app/src/main/java/com/appvisor_event/master/Constants.java
+++ app/src/main/java/com/appvisor_event/master/Constants.java
@@ -35,6 +35,6 @@ public class Constants {
     // device_tokenのためのAPI
     public static final String DEVICE_TOKEN_API_URL = BASE_URL + Event + "/api/users/update.json";
     //PUSHの設定値
-    public static final String GCM_BASE_URL = "";
-    public static final String GCM_SENDER_ID = "";
+    public static final String GCM_BASE_URL = "https://push.appvisor-event.com/";
+    public static final String GCM_SENDER_ID = "800785308038";
 }

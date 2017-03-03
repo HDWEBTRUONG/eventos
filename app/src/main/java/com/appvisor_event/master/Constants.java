00bf179ad95844ec84270ebbab624e6eaad484f1
bsfuji <fujimoto@bravesoft.co.jp>
2017-03-03 19:00:15 +0900

    mod 更新漏れの修正。

--- app/src/main/java/com/appvisor_event/master/Constants.java
+++ app/src/main/java/com/appvisor_event/master/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
     //アプリ内に表示させるドメイン
     public static final String APPLI_DOMAIN = "appvisor-event";
     //アプリ内に表示させるドメイン
-    public static final String EXHIBITER_DOMAIN_1 = "www.edogawa-u.ac.jp";
+    public static final String EXHIBITER_DOMAIN_1 = "edogawa-u.ac.jp";
     public static final String EXHIBITER_DOMAIN_2 = "XXXXXXXX";
     public static final String EXHIBITER_DOMAIN_3 = "XXXXXXXX";
     public static final String EXHIBITER_DOMAIN_4 = "XXXXXXXX";

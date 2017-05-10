e441f6dd4bca332482f6fb2f698de00b483d025b
大熊涼太 <ookuma.bravesoft@gmail.com>
2017-05-10 17:41:12 +0900

    SAPフォーラム2017でも作成

--- app/src/main/java/com/appvisor_event/master/Constants.java
+++ app/src/main/java/com/appvisor_event/master/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 
     private Constants(){}
     //ホームのURL
-    public static final String Event = "edogawa-u";
+    public static final String Event = "sap-forum2017";
     //ベースURL
     public static final String BASE_URL = "https://stg-api.appvisor-event.com/";
     //ホームのURL
@@ -25,7 +25,7 @@ public class Constants {
     //アプリ内に表示させるドメイン
     public static final String APPLI_DOMAIN = "appvisor-event";
     //アプリ内に表示させるドメイン
-    public static final String EXHIBITER_DOMAIN_1 = "edogawa-u.ac.jp";
+    public static final String EXHIBITER_DOMAIN_1 = "XXXXXXXX";
     public static final String EXHIBITER_DOMAIN_2 = "XXXXXXXX";
     public static final String EXHIBITER_DOMAIN_3 = "XXXXXXXX";
     public static final String EXHIBITER_DOMAIN_4 = "XXXXXXXX";

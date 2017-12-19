b9e806766043a36e6a1fd4ec5cb17c346983389a
レ　ミン　アン　カイ <l.khai@bravesoft.co.jp>
2017-12-19 12:34:03 +0900

    Remove photoframe onpagestarted handling

--- app/src/main/java/com/appvisor_event/master/MainActivity.java
+++ app/src/main/java/com/appvisor_event/master/MainActivity.java
@@ -795,33 +795,6 @@ public class MainActivity extends BaseActivity implements AppPermission.Interfac
                     findViewById(R.id.webView1).setVisibility(View.VISIBLE);
                     //エラーページを非表示にする
                     findViewById(R.id.error_page).setVisibility(View.INVISIBLE);
-
-                } else if (url.indexOf(Constants.PhotoframeUrl()) != -1) {
-                    String p = MainActivity.this.getFilesDir().toString() + "/images";
-                    File file=new File(p);
-                    if (!file.exists()){
-                        showCameradialog();
-                    }else {
-                        Intent intent = new Intent(MainActivity.this, CameraSquareActivity.class);//getApplication()
-
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            int checkCallPhonePermission = ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CAMERA);
-                            if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
-                                requestPermissions(new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_CAMERA);
-                                return;
-                            } else {
-                                startActivity(intent);
-                            }
-
-                        } else {
-                            startActivity(intent);
-                        }
-                    }
-                } else if (url.indexOf(Constants.FACEBOOK_PHOTO_URL) != -1){
-                    // インテントのインスタンス生成
-                    Intent intent = new Intent(MainActivity.this, FacebookPhotoActivity.class);
-                    // サブ画面の呼び出し
-                    startActivity(intent);
                 } else {
                     active_url = url;
                     if ((active_url.indexOf(Constants.RegARFlag) != -1)) {
